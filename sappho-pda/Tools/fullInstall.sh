03ff19f5479c6e9f93fcb1f8b3941db58c2766e7
Bandis Asterios <68644945+bandisast@users.noreply.github.com>
2022-08-12 17:15:42 +0300

    Update fullInstall.sh

--- sappho-pda/Tools/fullInstall.sh
+++ sappho-pda/Tools/fullInstall.sh
@@ -4,15 +4,6 @@ mkdir -m 777 /home/debian/sappho-pda/Code/Build
 chmod -R 777 /home/debian/sappho-pda
 rm /home/debian/sappho-pda/Samples/README.md
 
-#rm /lib/firmware/sappho-pda-00A0.dtbo
-#dtc -O dtb -o sappho-pda-00A0.dtbo -b 0 -@ sappho-pda.dts
-#sudo cp sappho-pda-00A0.dtbo /lib/firmware
-#echo "Activating SAPPHO cape..."
-#modprobe uio_pruss
-#echo sappho-pda > /sys/devices/platform/bone_capemgr/slots
-#sleep .5
-#echo "uio_pruss and SAPPHO cape enabled."
-
 pasm -b /home/debian/sappho-pda/Code/PRU/PRU0.p
 sleep .1
 pasm -b /home/debian/sappho-pda/Code/PRU/PRU1.p
